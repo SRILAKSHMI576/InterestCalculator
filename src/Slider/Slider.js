0bc831ed40c3794722038be50a1bcdd7bc121392
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 12:48:44 +0530

    Capture slider value

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -2,15 +2,36 @@ import React from "react";
 import "./style.css";
 
 class Slider extends React.Component {
+  state = {
+    value: 70
+  };
+  changeValue = value => {
+    this.setState({
+      value: value
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="slider-result">
           <h3>Amount</h3>
-          <input className="result" type="text" value="1000" disabled />
+          <input
+            className="result"
+            type="text"
+            value={this.state.value}
+            disabled
+          />
         </div>
         <br />
-        <input type="range" min="1" max="100" value="50" className="slider" />
+        <input
+          type="range"
+          min="1"
+          max="100"
+          onChange={e => this.changeValue(e.target.value)}
+          value={this.state.value}
+          className="slider"
+        />
       </div>
     );
   }
