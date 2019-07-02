7b7bb928543cbfa93ad65fdbb3de063c6b73a0c4
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 12:12:07 +0530

    Create slider

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./style.css";
 
 class Slider extends React.Component {
   render() {
-    return <div>slider</div>;
+    return (
+      <div>
+        <h3>Amount</h3>
+        <input type="text" value="1000" disabled />
+        <br />
+        <br />
+        <input type="range" min="1" max="100" value="50" className="slider" />
+      </div>
+    );
   }
 }
 export default Slider;
