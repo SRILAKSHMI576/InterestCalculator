12896c94c629ae63df9c5ce9ed1a744ef9039a6f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 12:21:32 +0530

    Apply styles for input

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -5,9 +5,10 @@ class Slider extends React.Component {
   render() {
     return (
       <div>
-        <h3>Amount</h3>
-        <input type="text" value="1000" disabled />
-        <br />
+        <div className="slider-result">
+          <h3>Amount</h3>
+          <input className="result" type="text" value="1000" disabled />
+        </div>
         <br />
         <input type="range" min="1" max="100" value="50" className="slider" />
       </div>
