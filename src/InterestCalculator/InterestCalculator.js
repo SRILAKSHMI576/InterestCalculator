8bba558b293da81e0073ac238ee9129c9678ec58
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 13:06:03 +0530

    Create Result component

--- src/InterestCalculator/InterestCalculator.js
+++ src/InterestCalculator/InterestCalculator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Slider from "../Slider/Slider";
+import Result from "../Result/Result";
 
 class InterestCalculator extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class InterestCalculator extends React.Component {
       <div>
         <Slider title="Amount" min="500" max="5000" />
         <Slider title="Loan duration" min="6" max="24" />
+        <Result />
       </div>
     );
   }
