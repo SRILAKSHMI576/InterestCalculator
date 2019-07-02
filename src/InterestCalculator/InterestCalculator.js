7a003e6ac5d480a6415de2b40fe85f45ea13f003
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 20:22:43 +0530

    Make API cal to get results

--- src/InterestCalculator/InterestCalculator.js
+++ src/InterestCalculator/InterestCalculator.js
@@ -2,8 +2,28 @@ import React from "react";
 import Slider from "../Slider/Slider";
 import Result from "../Result/Result";
 import "./style.css";
+import { async } from "q";
 
 class InterestCalculator extends React.Component {
+  state = {
+    loan: 6,
+    amount: 500,
+    interestRate: 0,
+    monthlyPayment: 0
+  };
+  componentDidMount = () => {
+    this.getAmount();
+  };
+  getAmount = async () => {
+    const response = await fetch(
+      "https://ftl-frontend-test.herokuapp.com/interest?amount=2000&numMonths=12"
+    );
+    const data = await response.json();
+    this.setState({
+      interestRate: data.interestRate,
+      monthlyPayment: data.monthlyPayment.amount
+    });
+  };
   render() {
     return (
       <div className="interest-calculator">
@@ -11,7 +31,10 @@ class InterestCalculator extends React.Component {
           <Slider title="Amount" min="500" max="5000" />
           <Slider title="Loan duration" min="6" max="24" />
         </div>
-        <Result interestRate={0.5} monthlyPayment={100} />
+        <Result
+          interestRate={this.state.interestRate}
+          monthlyPayment={this.state.monthlyPayment}
+        />
       </div>
     );
   }
