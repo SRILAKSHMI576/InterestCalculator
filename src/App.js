96d1df52b97f44c8a17b778c1c1e8884a3cf61b6
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 11:46:46 +0530

    Create InterestCalculator project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InterestCalculator from "./InterestCalculator/InterestCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <InterestCalculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/InterestCalculator/InterestCalculator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class InterestCalculator extends React.Component {
+  render() {
+    return <div>hi</div>;
+  }
+}
+
+export default InterestCalculator;
