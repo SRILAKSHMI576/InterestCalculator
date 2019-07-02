4e0aaed549303d20e9f3f8f3feaec45cce301045
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-02 15:01:24 +0530

    Update changes in interestCalculator

--- src/InterestCalculator/InterestCalculator.js
+++ src/InterestCalculator/InterestCalculator.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Slider from "../Slider/Slider";
 import Result from "../Result/Result";
+import "./style.css";
 
 class InterestCalculator extends React.Component {
   render() {
     return (
-      <div>
-        <Slider title="Amount" min="500" max="5000" />
-        <Slider title="Loan duration" min="6" max="24" />
+      <div className="interest-calculator">
+        <div>
+          <Slider title="Amount" min="500" max="5000" />
+          <Slider title="Loan duration" min="6" max="24" />
+        </div>
         <Result />
       </div>
     );
